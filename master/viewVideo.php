c0600a9f92d9499f43997692681759a25c7e8642
VishnuVandith <39836113+VishnuVandith@users.noreply.github.com>
2020-08-25 01:29:40 +0530

    php files

---
+++ master/viewVideo.php
@@ -0,0 +1,31 @@
+<html>
+        <body>
+            
+        
+        <video id="eduVideo" width="320" height="240">
+            <source src="movie.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+        </video>
+        <button onclick="playVideo()">Play</button>
+        <button onclick="replayFunction()">Replay</button>
+
+        </body>
+
+<script>
+     
+        function playVideo(){
+            var x = document.getElementById("eduVideo");
+            var playCount=1;
+            x.play();
+        }
+        
+        function replayFunction(){
+            var count =1;
+            count=count+1;
+            document
+        }
+      
+</script>
+
+</html>
